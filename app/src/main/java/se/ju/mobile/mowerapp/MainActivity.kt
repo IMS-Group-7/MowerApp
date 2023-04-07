b308c8ac81861cda2bc8a87bb3211f01cd62be43
aljonasamuelsson <aljonasamuelsson@hotmail.com>
2023-04-07 23:29:03 +0200

    updated build.gradle file

--- app/src/main/java/se/ju/mobile/mowerapp/MainActivity.kt
+++ app/src/main/java/se/ju/mobile/mowerapp/MainActivity.kt
@@ -1,16 +1,24 @@
 package se.ju.mobile.mowerapp
-
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.annotation.RequiresApi
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
+
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import se.ju.mobile.mowerapp.ui.theme.MowerAppTheme
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
