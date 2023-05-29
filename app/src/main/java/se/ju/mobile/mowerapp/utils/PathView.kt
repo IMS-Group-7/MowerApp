99840a3cfca499684f715b7be39cdb8697252722
TypoTami <up2010614@myport.ac.uk>
2023-05-29 22:34:48 +0200

    Beginnings of path showy thingy

--- app/src/main/java/se/ju/mobile/mowerapp/utils/PathView.kt
+++ app/src/main/java/se/ju/mobile/mowerapp/utils/PathView.kt
@@ -10,12 +10,16 @@ import android.util.AttributeSet
 import android.view.View
 import kotlinx.coroutines.runBlocking
 import org.json.JSONArray
+import kotlin.math.cos
+import kotlin.math.sin
 
 class PathView : View {
     private var pathPaint: Paint? = null
     private var path: Path? = null
     private var boundariesPaint: Paint? = null
     private var boundaries: Path? = null
+
+    private var number: Float = 0f
 
     constructor(context: Context?) : super(context) {
         init()
@@ -44,6 +48,8 @@ class PathView : View {
         boundariesPaint!!.style = Paint.Style.STROKE
         boundariesPaint!!.strokeWidth = 7F
         boundaries = Path()
+
+        number = 20f
     }
 
     @SuppressLint("DrawAllocation")
@@ -53,38 +59,51 @@ class PathView : View {
         val boundariesPoints = ArrayList<PointF>()
         val res = ApiManager()
 
-        // Draws the boundaries
-        runBlocking {
-            val asyncResponse = res.makeHttpGetRequest("http://34.173.248.99/coordinates/boundaries")
-            val jsonResponse = JSONArray(asyncResponse)
-            for (i in 0 until jsonResponse.length()) {
-                boundariesPoints.add(
-                    PointF(
-                        jsonResponse.getJSONObject(i).getString("x").toFloat(),
-                        jsonResponse.getJSONObject(i).getString("y").toFloat()
-                    )
-                )
-            }
-            for (i in 0 until boundariesPoints.size) {
-                val point: PointF = boundariesPoints[i]
-                if (i == 0) {
-                    boundaries?.moveTo(point.x, point.y)
-                } else {
-                    boundaries?.lineTo(point.x, point.y)
-                }
-            }
-            canvas?.drawPath(boundaries!!, boundariesPaint!!)
+        val paintUse = this.pathPaint
+
+        postInvalidateDelayed(10)
+
+        number += 0.1f
+
+        canvas.apply {
+            paintUse?.let { this?.drawCircle(500f + sin(number.toDouble()).toFloat()*200f,
+                300f + cos(number.toDouble()/4f).toFloat()*200f, 35f, it) }
         }
 
-        // Draw the path
-        for (i in 0 until pathPoints.size) {
-            val point: PointF = pathPoints[i]
-            if (i == 0) {
-                path?.moveTo(point.x, point.y)
-            } else {
-                path?.lineTo(point.x, point.y)
-            }
-        }
-        canvas?.drawPath(path!!, pathPaint!!)
+//        invalidate()
+
+        // Draws the boundaries
+//        runBlocking {
+//            val asyncResponse = res.makeHttpGetRequest("http://34.173.248.99/coordinates/boundaries")
+//            val jsonResponse = JSONArray(asyncResponse)
+//            for (i in 0 until jsonResponse.length()) {
+//                boundariesPoints.add(
+//                    PointF(
+//                        jsonResponse.getJSONObject(i).getString("x").toFloat(),
+//                        jsonResponse.getJSONObject(i).getString("y").toFloat()
+//                    )
+//                )
+//            }
+//            for (i in 0 until boundariesPoints.size) {
+//                val point: PointF = boundariesPoints[i]
+//                if (i == 0) {
+//                    boundaries?.moveTo(point.x, point.y)
+//                } else {
+//                    boundaries?.lineTo(point.x, point.y)
+//                }
+//            }
+//            canvas?.drawPath(boundaries!!, boundariesPaint!!)
+//        }
+//
+//        // Draw the path
+//        for (i in 0 until pathPoints.size) {
+//            val point: PointF = pathPoints[i]
+//            if (i == 0) {
+//                path?.moveTo(point.x, point.y)
+//            } else {
+//                path?.lineTo(point.x, point.y)
+//            }
+//        }
+//        canvas?.drawPath(path!!, pathPaint!!)
     }
 }
