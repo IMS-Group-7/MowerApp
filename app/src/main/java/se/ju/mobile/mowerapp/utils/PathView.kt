d06c54ee01bbcbdff92d0af3baba7db91a00355d
TypoTami <up2010614@myport.ac.uk>
2023-05-30 02:44:48 +0200

    (with dummy data) Path can now be drawn to the app, and updated. Additionally path is scaled to the currently window size

--- app/src/main/java/se/ju/mobile/mowerapp/utils/PathView.kt
+++ app/src/main/java/se/ju/mobile/mowerapp/utils/PathView.kt
@@ -2,10 +2,7 @@ package se.ju.mobile.mowerapp.utils
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Path
-import android.graphics.PointF
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 import kotlinx.coroutines.runBlocking
@@ -20,6 +17,7 @@ class PathView : View {
     private var boundaries: Path? = null
 
     private var number: Float = 0f
+    private var pathPoints: ArrayList<PointF> = ArrayList<PointF>()
 
     constructor(context: Context?) : super(context) {
         init()
@@ -42,7 +40,7 @@ class PathView : View {
         pathPaint!!.color = Color.BLUE
         pathPaint!!.style = Paint.Style.STROKE
         pathPaint!!.strokeWidth = 5F
-        path = Path()
+//        path = Path()
         boundariesPaint = Paint()
         boundariesPaint!!.color = Color.BLACK
         boundariesPaint!!.style = Paint.Style.STROKE
@@ -50,24 +48,77 @@ class PathView : View {
         boundaries = Path()
 
         number = 20f
+
+        pathPoints = ArrayList<PointF>()
+        pathPoints.add(PointF(9f, 15f))
+        pathPoints.add(PointF(24f, 17f))
+        pathPoints.add(PointF(27f, 25f))
+        pathPoints.add(PointF(35f, 15f))
+        pathPoints.add(PointF(40f, 20f))
     }
 
     @SuppressLint("DrawAllocation")
     override fun onDraw(canvas: android.graphics.Canvas?) {
         super.onDraw(canvas)
-        val pathPoints = ArrayList<PointF>()
+//        val pathPoints = ArrayList<PointF>()
+        val pathPoints = this.pathPoints
         val boundariesPoints = ArrayList<PointF>()
         val res = ApiManager()
+        val bounds = RectF()
+        val boundsScaleMatrix = Matrix()
+        val path = Path()
+        var padValue = 40f
+        var scalar = 1f
+        val pathPaint = this.pathPaint
 
-        val paintUse = this.pathPaint
+        postInvalidateDelayed(500)
 
-        postInvalidateDelayed(10)
+        // Draw the path
+        for (i in 0 until pathPoints.size) {
+            val point: PointF = pathPoints[i]
 
-        number += 0.1f
+            var scaledPoint = PointF(
+                point.x,
+                point.y
+            )
 
+            if (i == 0) {
+                path?.moveTo(scaledPoint.x, scaledPoint.y)
+            } else {
+                path?.lineTo(scaledPoint.x, scaledPoint.y)
+            }
+        }
+
+        // Compute bounding box of path and derive scalar
+        path?.computeBounds(bounds, true)
+
+        if (bounds.width() > bounds.height()) {
+            scalar = (width - padValue * 2) / bounds.width()
+        } else {
+            scalar = (height - padValue * 2) / bounds.height()
+        }
+
+        // Scale the path to the screen
+        boundsScaleMatrix.setScale(
+            scalar,
+            -scalar,
+            bounds.centerX(),
+            bounds.centerY()
+        )
+
+        path?.transform(boundsScaleMatrix)
+
+        // Center it
+        boundsScaleMatrix.setTranslate(
+            -bounds.centerX() + width  / 2,
+            -bounds.centerY() + height / 2
+        )
+
+        path?.transform(boundsScaleMatrix)
+
+        // Finally draw
         canvas.apply {
-            paintUse?.let { this?.drawCircle(500f + sin(number.toDouble()).toFloat()*200f,
-                300f + cos(number.toDouble()/4f).toFloat()*200f, 35f, it) }
+            this?.drawPath(path!!, pathPaint!!)
         }
 
 //        invalidate()
